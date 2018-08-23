3bfac605642c22d113d5f7abf0ef875aeba562c8
Eduardo Souza <udesou@gmail.com>
2018-08-23 17:08:47 +1000

    Adding Spoofax Sunshine jar, and removing project.

--- org.metaborg.sunshine2/src/main/java/org/metaborg/sunshine/arguments/MainArguments.java
+++
@@ -1,9 +0,0 @@
-package org.metaborg.sunshine.arguments;
-
-import com.beust.jcommander.Parameter;
-
-public class MainArguments {
-    @Parameter(names = { "-h", "--help" }, description = "Shows this usage information", help = true) public boolean help;
-
-    @Parameter(names = { "--exit" }, description = "Immediately exit, used for testing purposes", hidden = true) public boolean exit;
-}
